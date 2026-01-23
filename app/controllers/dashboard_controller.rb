763da9fe04d3c83f37f2328a04c4613112f3eac1
Christopher Funk <christopher.funk@ascendlearning.com>
2026-01-23 09:55:48 -0500

    Updates to help admins find membership owners

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,13 @@
 
 class DashboardController < ApplicationController
   def index
-    @is_group_admin = current_user.memberships.any?(&:is_group_admin?)
-    @admin_groups = current_user.memberships.select(&:is_group_admin?).map(&:wedding_group) if @is_group_admin
+    @current_group = WeddingGroup.find_by(id: session[:current_group_id])
+    if @current_group
+      membership = @current_group.memberships.find_by(user_id: current_user.id)
+      @is_group_admin = membership&.is_group_admin?
+    else
+      @is_group_admin = false
+    end
+
   end
 end
